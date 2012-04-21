3f1cdb8911b840a42137f2bac2ccbf328ad1190d
Alex Wright <alex@xeentech.com>
2012-04-21 18:23:24 +0000

    More DRY.

--- app/models/profile_model.php
+++ app/models/profile_model.php
@@ -1,27 +1,37 @@
 <?php
 class Profile_model extends MY_Model {
+    private $table_name = 'profile';
+
     public function find_by_id ($id)
     {
-        $query = $this->db->get_where('profile', array('id'=> $id));
-        if ($query->num_rows() == 0)
-        {
-            return FALSE;
-        }
-        return $query->row();
+        return $this->row_or_false($this->get_where(array(
+            'id'        => $id,
+        )));
     }
 
     public function find_by_email ($email_address)
     {
         list($local, $domain) = explode('@', $email_address, 2);
-        $query = $this->db->get_where('profile', array(
+        return $this->row_or_false($this->get_where(array(
             'local'     => $local,
             'domain'    => $domain,
-        ));
-        if ($query->num_rows() == 0)
+        )));
+    }
+
+    private function get_where ($where)
+    {
+        return $this->db->get_where(
+            $this->table_name,
+            $where);
+    }
+
+    private function row_or_false ($query_ref)
+    {
+        if ($query_ref->num_rows() == 0)
         {
             return FALSE;
         }
-        return $query->row();
+        return $query_ref->row();
     }
 }
 
