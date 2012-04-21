685ea2200ffb57d45f1b8127c5c37f5d2f9d8449
Alex Wright <alex@xeentech.com>
2012-04-21 18:15:34 +0000

    DB setup.

--- app/config/database.php
+++ app/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'alex_diasphp';
+$db['default']['password'] = 'IgtmgFVtDj7Q';
+$db['default']['database'] = 'alex_diasphp';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
