a90cb666bcd1716abfd4fdf522aa9f6bcf21e73b
Alex Wright <alex@xeentech.com>
2012-05-17 21:52:49 +0000

    Testing out an idea to have methods for each request type, get/post/etc.

--- app/core/MY_Controller.php
+++ app/core/MY_Controller.php
@@ -14,4 +14,26 @@ class MY_Controller extends CI_Controller {
             $this->user = $this->user_model->get_by_id($user_id);
         }
     }
+
+    public function _remap ($method, $params)
+    {
+        $request_method = strtolower($this->input->server('REQUEST_METHOD'));
+
+        if (method_exists($this, $request_method . '_' . $method))
+        {
+            $method = $request_method . '_' . $method;
+        }
+
+        $r = new ReflectionMethod($this, $method);
+        if ($r->isPublic() && !$r->isStatic())
+        {
+            return call_user_func_array(array($this, $method), $params);
+        }
+        
+        header('Content-Type: text/plain');
+        var_dump(array(
+            $method,
+            $params,
+        ));
+    }
 }
