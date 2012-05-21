8f34718017b250bc4471dcbeeae06a345df69453
Alex Wright <alex@xeentech.com>
2012-05-21 13:27:30 +0000

    More nonsense tripping up the XML Parser.

--- app/controllers/receive.php
+++ app/controllers/receive.php
@@ -83,7 +83,7 @@ class Receive extends CI_Controller {
         $dom = new DOMDocument();
         $dom->preserveWhiteSpace = FALSE;
 
-        $xml = str_replace(array(chr(12)), '', $xml);
+        $xml = str_replace(array(chr(12), chr(3)), '', $xml);
         $xml = trim($xml);
         $dom->loadXML($xml);
         return $dom;
