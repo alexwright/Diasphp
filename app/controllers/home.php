81dfeef50c5b5e24758b1bed2d5b835fa51d968c
Alex Wright <alex@xeentech.com>
2012-05-21 12:07:39 +0000

    Redirect to login if the user isn't logged in.

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -2,6 +2,10 @@
 class Home extends MY_Controller {
     public function index ()
     {
+        if ( ! $this->user )
+        {
+            return redirect('login');
+        }
         $this->view('home/main');
     }
 }
